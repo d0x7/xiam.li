705f8f35a8e83361162aef6977c50eae3e68f1c9
Dorian Heinrichs <dorian.heinrichs@xiamli.com>
2024-08-21 20:27:12 +0200

    Updated image assets

--- ui/src/app/__opengraph/page.js
+++ ui/src/app/__opengraph/page.js
@@ -4,13 +4,13 @@ import { cn } from "@/lib/utils";
 const debug = false
 
 export default function OpengraphPage() {
-    return (
-      <div className={cn("h-[315px] w-[600px] flex flex-row align-center justify-center items-center mx-5", debug ? "border-red-500 border-2" : "")}>
-          <img src="/assets/pp.jpeg" width={100} height={100} className="rounded-full" />
+    return ( // Screenshot taken at 4k res on 14" MBP with 175% scaling in Arc, then centered in Pixelmator Pro
+      <div className={cn("pt-96 flex flex-row align-center justify-center items-center mx-5", debug ? "border-red-500 border-2" : "")}>
+          <img src="/assets/pp.png" width={100} height={100}/>
           <Separator orientation="vertical" className="mx-5 bg-gray-600 h-20" />
           <div>
             <div className="text-4xl">xiam.li</div>
-            <Separator className="mt-4 mb-2 bg-gray-600" />
+                <Separator className="mt-4 mb-2 bg-gray-600" />
             <div className="text-lg text-muted-foreground">Xiam.Li Go Package Index</div>
           </div>
       </div>
